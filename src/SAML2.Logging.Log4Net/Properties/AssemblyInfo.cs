473ceb4ffe48a1754ec96ba7ee15a3efbaa5e5ba
Michael Hallock <i8beef@users.noreply.github.com>
2013-08-25 13:27:20 -0400

    Abstracted out NuGet package creation and updated to require SAML 2.3.1 as a base.

--- src/SAML2.Logging.Log4Net/Properties/AssemblyInfo.cs
+++ src/SAML2.Logging.Log4Net/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SAML2.Loggin.Log4Net")]
 [assembly: AssemblyDescription("A logging provider for SAML2 based on Log4Net.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Hallock")]
 [assembly: AssemblyProduct("SAML2.Loggin.CommonLogging")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.5")]
-[assembly: AssemblyFileVersion("1.0.0.5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
